91d7d041d3a503af1460689bbf9fad91193df932
alexandrerosapc <alexandrerosasoares3@gmail.com>
2024-08-30 08:52:48 -0300

    mais algumas rotas criadas

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import { MongoClient, ObjectId } from "mongodb";
 import dotenv from "dotenv";
 import joi from "joi";
+import dayjs from "dayjs";
 
 dotenv.config();
 
@@ -12,6 +13,13 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+server.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=UTF-8');
+  req.headers['accept-charset'] = 'utf-8'; // Adiciona o charset ao header de requisição
+  next();
+});
+
 
 let db;
 
@@ -48,14 +56,95 @@ server.post("/participants", async (req, res) => {
 
     if (participanteExiste) return res.status(409).send("Este usuário já existe!");
 
-    const result = await db.collection("participantes").insertOne({ name: participante.name });
-    
-    res.status(201).send({ id: result.insertedId, message: "Participante criado com sucesso" });
+    const lastStatus = Date.now();
+
+    const participanteComStatus = {
+      name: participante.name,
+      lastStatus // utilize o timestamp original
+    };
+
+    await db.collection("participantes").insertOne(participanteComStatus);
+
+    const message = {
+      from: participante.name,
+      to: 'Todos',
+      text: 'entra na sala...',
+      type: 'status',
+      time: dayjs(lastStatus).format('HH:mm:ss') // Aqui o tempo formatado
+    };
+
+    await db.collection("messages").insertOne(message);
+
+    res.sendStatus(201);
   } catch (error) {
-    console.log("Erro ao inserir participante:", error);
+    console.log("Erro ao inserir participante e/ou mensagem:", error);
     res.status(500).send("Erro no servidor");
   }
 });
 
+server.get("/participants", async (req, res) => {
+  try {
+    const dados = await db.collection("participantes").find().toArray();
+    console.log(dados);
+
+    return res.send(dados);
+  } catch (error) {
+    res.status(500).send("Deu erro no servidor de banco de dados");
+  }
+});
+
+server.post("/messages", async (req, res) => {
+  const message = req.body;
+  const { from } = req.headers;
+
+  console.log(from)
+
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.string().valid("message", "private_message").required(),
+  });
+
+  // Validação do objeto message diretamente
+  const validation = messageSchema.validate(message);
+
+  if (validation.error) {
+    const erros = validation.error.details.map((err) => err.message);
+    return res.status(422).send(erros);
+  }
+
+  try {
+    const participanteExiste = await db.collection("participantes").findOne({ name: from });
+
+    if (!participanteExiste) {
+      return res.status(422).send("Usuário não faz parte do grupo");
+    }
+
+    const time = dayjs().format("HH:mm:ss");
+
+    const novaMensagem = {
+      ...message,
+      from,
+      time,
+    };
+
+    await db.collection("messages").insertOne(novaMensagem);
+    res.sendStatus(201);
+  } catch (error) {
+    console.log("Erro ao inserir participante e/ou mensagem:", error);
+    res.status(500).send("Erro no servidor");
+  }
+});
+
+server.get("/messages", async (req, res) => {
+  try {
+    const dados = await db.collection("messages").find().toArray();
+    console.log(dados);
+
+    return res.send(dados);
+  } catch (error) {
+    res.status(500).send("Deu erro no servidor de banco de dados");
+  }
+});
 
 startServer();
